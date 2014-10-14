c5cc78eeb0ce743f4b8ee410aa46884fb1a58176
Giuseppe Crinò <giuscri@gmail.com>
2014-10-14 23:19:22 +0200

    Fuuuu...
    Some spaghetti code! But at least it helped me better understanding
    C pointers!!

--- how_many.c
+++ how_many.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 void printarray_Int(int *v, int v_length) {
 	printf("[ ");
@@ -10,98 +11,112 @@ void printarray_Int(int *v, int v_length) {
 	printf("]");
 }
 
-int main () {
-	printf("Type in a number: ");
-	int in_number;
-	#ifdef DEBUG
-		in_number = 123;
-		printf("%d\n", in_number);
-	#else
-		scanf("%d", &in_number);
-	#endif
-	// Count the number of digits via dividing
-	// by 10. The number of times needed to
-	// obtain 0, is the number of digits
-	int digits_length = 0;
-	for (int i = 1, tmp_number = in_number;; i++) {
+void initializearray_Int(int **v, int v_length, int initializing_value) {
+	for (int i = 0; i < v_length; i++) {
+		(*v)[i] = initializing_value;
+	}
+}
+
+void resizearray_Int(int **v, int v_length, int desired_length) {
+	if (desired_length > v_length) {
+		printf("resizearray_Int: cannot resize to longer length.\n");
+		exit(1);
+	}
+	int *tmp = malloc(sizeof(int) * desired_length);
+	for (int i = 0; i < desired_length; i++) {
+		tmp[i] = (*v)[i];
+	}
+	*v = tmp;
+}
+
+int countdigits_Int (int number) {
+	int number_of_digits = 0;
+	for (int i = 1, tmp_number = number;; i++) {
 		tmp_number /= 10;
 		if (tmp_number < 1) {
-			digits_length = i;
+			number_of_digits = i;
 			break;
 		}
 	}
-	#ifdef DEBUG
-		printf("[DEBUG] digits_length: %d\n", digits_length);
-	#endif
+	return number_of_digits;
+}
+
+void fillarraywith_Int (int **digits, int *digits_length, int number) {
+	*digits_length = countdigits_Int(number);
 	// Create an array of length <digits_length>
-	int *digits = malloc(sizeof(int) * digits_length);
-	// Fill it with the digits of in_number via %10'ing
-	for (int i = 0, tmp_number = in_number; i < digits_length; i++) {
-		digits[i] = tmp_number % 10;
+	*digits = malloc(sizeof(int) * *digits_length);
+	// Fill it with the digits of number via %10'ing
+	for (int i = 0, tmp_number = number; i < *digits_length; i++) {
+		(*digits)[i] = tmp_number % 10;
 		tmp_number /= 10;
 		if (tmp_number < 1) {
 			break;
 		}
 	}
-	#ifdef DEBUG
-		printf("[DEBUG] digits: ");
-		printarray_Int(digits, digits_length);
-		printf("\n");
-	#endif
+}
+
+void fillarraywithduplicatesfrom_Int (int **duplicates, int *duplicates_length, int *digits, int digits_length) {
 	// Create a new array of length <number_of_digits>
-	int *duplicates = malloc(sizeof(int) * digits_length);
-	int duplicates_length = digits_length;
+	// since duplicates are <= <number_of_digits>
+	*duplicates = malloc(sizeof(int) * digits_length);
+	*duplicates_length = digits_length;
+	// ...
+	initializearray_Int(duplicates, *duplicates_length, 0);
 	// Traversing over the first array, fill the second
 	// only with the duplicates in the first AND
 	// if not already in the second
 	bool already_in = false;
 	// Iterating over the digits array ...
-	for (int i = 0; i < digits_length; i++) {
+	for (int i = 0, h = 0; i < digits_length; i++) {
 		for (int j = i + 1; j < digits_length; j++) {
 			// If found a duplicate ...
 			if (digits[i] == digits[j]) {
 				// ... that's not already loaded ...
 				for (int k = i - 1; k >= 0; k--) {
-					if (digits[i] == duplicates[k]) {
+					if (digits[i] == (*duplicates)[k]) {
 						already_in = true;
 						break;
 					}
 				}
 				// ... save it into duplicates!
 				if (!already_in) {
-					#ifdef DEBUG
-						printf("[DEBUG] %d already in!\n", digits[i]);
-					#endif
-					duplicates[i] = digits[i];
+					(*duplicates)[h] = digits[i];
+					h = h + 1;
+					*duplicates_length = h;
 					break;
 				}
 			}
 		}
 	}
-	bool duplicates_empty = true;
+	// Resizing, using digits_lengt as initial length
+	// since we allocated digits_length * sizeof(int) bytes
+	resizearray_Int(duplicates, digits_length, *duplicates_length);
+}
+
+void how_many (int number, int **duplicates, int *duplicates_length) {
+	// Count the number of digits via dividing
+	// by 10. The number of times needed to
+	// obtain 0, is the number of digits
+	int digits_length;
+	int *digits;
+	fillarraywith_Int(&digits, &digits_length, number);
+	// Initialize the duplicates array with 0's
+	fillarraywithduplicatesfrom_Int(duplicates, duplicates_length, digits, digits_length);
+	free(digits);
+}
+
+int main () {
+	int *duplicates;
+	int duplicates_length;
+	how_many(33010, &duplicates, &duplicates_length);
+	printarray_Int(duplicates, duplicates_length);
+	printf("\n");
+	int testerarray[] = { 0, 3 };
 	for (int i = 0; i < duplicates_length; i++) {
-		if (duplicates[i] != 0) {
-			duplicates_empty = false;
-			break;
+		if (testerarray[i] != duplicates[i]) {
+			assert(false && "Test on '33010' failed.");
 		}
-	}
-	// Print the second array if there are duplicates
-	if (!duplicates_empty) {
-		printf("Initial number has duplicates.\n");
-		//printarray_Int(duplicates, duplicates_length);
-		//printf("\n");
-		printf("[ ");
-		for (int i = 0; i < duplicates_length; i++) {
-			if (duplicates[i] != 0) {
-				printf("%d ", duplicates[i]);
-			}
-		}
-		printf("]\n");
-	}
-	else {
-		printf("Initiali number has NO duplicates.\n");
 	}
 	free(duplicates);
-	free(digits);
 	return 0;
 }
